123958f6e70988305eedabc06cd8b283e6f4a79a
quvntvn <hulkufe4@gmail.com>
2022-04-01 10:33:14 +0200

    MAJ Client

--- client/index.mjs
+++ client/index.mjs
@@ -1,16 +1,48 @@
-import axios from "axios"
+import axios from "axios";
 
 const getProducts = () => {
-    axios.post('https://localhost:3000')
-    .then((datas => ))
-    console.log("iife")
-}
+  setTimeout(() => {
+    return new Promise((resolve, reject) => {
+      axios.get("http://localhost:3011/products").then((response) => {
+        console.log(response.data);
+        resolve(response);
+      });
+    });
+  }, 1000);
+};
 
-const addProducts = () => {
-    console.log("iife")
-}
+const addProduct = (name, quantity) => {
+  setTimeout(() => {
+    axios
+      .post("http://localhost:3011/products", {
+        name: name,
+        quantity: quantity,
+      })
+      .then(
+        (response) => {
+          console.log(response.data);
+          return response;
+        },
+        (error) => {
+          return error;
+        }
+      );
+  }, 2000);
+};
 
 const getProductsAsync = () => {
-    console.log("iife")
-}
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      axios.get("http://localhost:3011/products").then((response) => {
+        resolve(response.data);
+      });
+    }, 3000);
+  });
+};
 
+(async () => {
+  getProducts();
+  addProduct("pomme", 15);
+  const res = await getProductsAsync();
+  console.log(res);
+})();
